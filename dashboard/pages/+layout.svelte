78159a3d8e14b5c73d7adb6900b3f610d1b319d6
Roxanna Rodriguez <roxanna.rdgz@gmail.com>
2026-05-30 18:24:55 +0300

    Update layout width and add footer

---
+++ dashboard/pages/+layout.svelte
@@ -0,0 +1,33 @@
+<script>
+	import "@evidence-dev/tailwind/fonts.css";
+	import "../app.css";
+	import { EvidenceDefaultLayout } from "@evidence-dev/core-components";
+	export let data;
+</script>
+
+<EvidenceDefaultLayout {data} maxWidth="1600">
+	<slot slot="content" />
+</EvidenceDefaultLayout>
+
+<footer
+	class="mt-16 border-t border-gray-200 pt-6 pb-8 text-sm text-gray-500 text-center"
+>
+	<p>
+		Built by Roxanna R ✨
+		<a
+			href="https://www.linkedin.com/in/roxannardgz/"
+			target="_blank"
+			class="underline"
+		>
+			LinkedIn
+		</a>
+		·
+		<a
+			href="https://github.com/roxannardgz/food-security-pipeline"
+			target="_blank"
+			class="underline"
+		>
+			GitHub Repo
+		</a>
+	</p>
+</footer>
